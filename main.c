e8292755dbb6b6898fce8d4d61cccac7a6f93c44
boom3146 <t7yjcv5txj@privaterelay.appleid.com>
2026-05-29 12:14:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -31,52 +32,84 @@ int main()
         return 1;
     }
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);//windows commends to give us control on terminal
+    DWORD consoleMode;
+    GetConsoleMode(hConsole, &consoleMode);
+    consoleMode |= 0x0004; 
+    SetConsoleMode(hConsole, consoleMode);
+
+    printf("Press ENTER to start the simulation!\n");
+    (void)getchar(); 
+
     CONSOLE_SCREEN_BUFFER_INFO csbi;
-    system("cls"); //cleans screen for map
+    GetConsoleScreenBufferInfo(hConsole, &csbi);
+    int screen_height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
 
-    printf("--- Starting Delivery DES Terminal Simulation ---\n");
-    printf("Legend: [.] Empty | [R] Restaurant | [C] Customer | [I] Idle Courier | [B] Busy Courier\n\n");
+    printf("\x1b[2J");                      // clean the screen
+    printf("\x1b[18;%dr", screen_height);   // lock lines 1-17
+    printf("\x1b[18;1H");
 
-    fprintf(log_file,"--- Starting Delivery DES Terminal Simulation ---\n");
-    fprintf(log_file,"Legend: [.] Empty | [R] Restaurant | [C] Customer | [I] Idle Courier | [B] Busy Courier\n\n");
+    fprintf(log_file, "--- Starting Delivery DES Terminal Simulation ---\n");
+    fprintf(log_file, "Legend: [.] Empty | [R] Restaurant | [C] Customer | [I] Idle Courier | [B] Busy Courier\n\n");
+
     event* curr_event = NULL;
     event* upcoming = NULL;
 
     //runs on events 
     while (sim->event_queue != NULL)
     {
+        if (_kbhit()) // check if was a keyboard click
+        {
+            char key = _getch(); 
+            if (key == ' ')
+            {
+                printf("\x1b[33m\n[!] SIMULATION PAUSED. Press SPACE to resume...\x1b[0m\n");
+                fflush(stdout);
+
+                while (1)
+                {
+                    if (_kbhit())
+                    {
+                        char resume_key = _getch();
+                        if (resume_key == ' ')
+                        {
+                            printf("\x1b[32m[!] SIMULATION RESUMED.\x1b[0m\n\n");
+                            fflush(stdout);
+                            break;
+                        }
+                    }
+                    Sleep(100); 
+                }
+            }
+        }
+
         upcoming = peek_event(sim->event_queue);//lets us see the upcoming event 
         if (upcoming != NULL) {
             // when an event is incomming 
             printf("[Radar] Next upcoming event is Type %d at T=%.2f...\n", upcoming->type, upcoming->time);
-            fprintf(log_file,"[Radar] Next upcoming event is Type %d at T=%.2f...\n", upcoming->type, upcoming->time);
+            fprintf(log_file, "[Radar] Next upcoming event is Type %d at T=%.2f...\n", upcoming->type, upcoming->time);
         }
         curr_event = dequeue_event(&sim->event_queue);
         if (curr_event == NULL) break;
 
-        sim->current_time = curr_event->time;
-        GetConsoleScreenBufferInfo(hConsole, &csbi);//saves the info before drawing the map again 
-        COORD log_pos = csbi.dwCursorPosition;
+       sim->current_time = curr_event->time;
+        
+        printf("\x1b[s");    // save the local place of the log (line 18 and down)
+        printf("\x1b[1;1H"); // jump to the top of the screen
 
-        //
-        COORD map_pos = { 0, csbi.srWindow.Top };//creates new map where the old one was
-        SetConsoleCursorPosition(hConsole, map_pos);
+        printf("--- Starting Delivery DES Terminal Simulation ---\x1b[K\n");
+        printf("Legend: [.] Empty | [R] Restaurant | [C] Customer | [I] Idle Courier | [B] Busy Courier | [SPACE] Pause\x1b[K\n");
 
-        // drawing the map and the commend are for the map to clear it self and update live while running
         draw_city_map(sim);
 
-        //
-        if (log_pos.Y < map_pos.Y + 14) {//saves the first 14 lines for map
-            log_pos.Y = map_pos.Y + 14;
-        }
-        SetConsoleCursorPosition(hConsole, log_pos);
+        printf("\x1b[u");    // return to the place we stop writing
+        
         //after drawing the map go dwon and keep wrtiting logs
         switch (curr_event->type)
         {
         case new_order:
             printf("[Time: %.2f] NEW ORDER: Order #%d was received in the system.\n", sim->current_time, curr_event->order_number);
 
-            fprintf(log_file,"[Time: %.2f] NEW ORDER: Order #%d was received in the system.\n", sim->current_time, curr_event->order_number);
+            fprintf(log_file, "[Time: %.2f] NEW ORDER: Order #%d was received in the system.\n", sim->current_time, curr_event->order_number);
 
             handle_new_order(sim, curr_event);
             break;
@@ -84,7 +117,7 @@ int main()
         case order_pickup:
             printf("[Time: %.2f] PICKUP: Courier is currently picking up Order #%d.\n", sim->current_time, curr_event->order_number);
 
-            fprintf(log_file,"[Time: %.2f] PICKUP: Courier is currently picking up Order #%d.\n", sim->current_time, curr_event->order_number);
+            fprintf(log_file, "[Time: %.2f] PICKUP: Courier is currently picking up Order #%d.\n", sim->current_time, curr_event->order_number);
 
             handle_order_pickup(sim, curr_event);
             break;
@@ -92,7 +125,7 @@ int main()
         case order_delivered:
             printf("[Time: %.2f] DELIVERED: Order #%d successfully reached the customer!\n", sim->current_time, curr_event->order_number);
 
-            fprintf(log_file,"[Time: %.2f] DELIVERED: Order #%d successfully reached the customer!\n", sim->current_time, curr_event->order_number);
+            fprintf(log_file, "[Time: %.2f] DELIVERED: Order #%d successfully reached the customer!\n", sim->current_time, curr_event->order_number);
 
             handle_order_delivered(sim, curr_event);
             break;
@@ -100,7 +133,7 @@ int main()
         case order_delayed:
             printf("[Time: %.2f] DELAY: Order #%d is experiencing a delay in traffic.\n", sim->current_time, curr_event->order_number);
 
-            fprintf(log_file,"[Time: %.2f] DELAY: Order #%d is experiencing a delay in traffic.\n", sim->current_time, curr_event->order_number);
+            fprintf(log_file, "[Time: %.2f] DELAY: Order #%d is experiencing a delay in traffic.\n", sim->current_time, curr_event->order_number);
 
             handle_order_delayed(sim, curr_event);
             break;
@@ -108,7 +141,7 @@ int main()
         case order_canceled:
             printf("[Time: %.2f] CANCELED: Order #%d has been canceled.\n", sim->current_time, curr_event->order_number);
 
-            fprintf(log_file,"[Time: %.2f] CANCELED: Order #%d has been canceled.\n", sim->current_time, curr_event->order_number);
+            fprintf(log_file, "[Time: %.2f] CANCELED: Order #%d has been canceled.\n", sim->current_time, curr_event->order_number);
 
             handle_order_canceled(sim, curr_event);
             break;
@@ -126,7 +159,7 @@ int main()
 
     printf("\n--- Simulation Complete ---\n");
 
-    fprintf(log_file,"\n--- Simulation Complete ---\n");
+    fprintf(log_file, "\n--- Simulation Complete ---\n");
 
     fclose(log_file);
     save_statistics_to_file(sim, "statistics_report.txt");
